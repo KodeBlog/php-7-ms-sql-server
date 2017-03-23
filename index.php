25f63c5e1c2ba7b1a87687299f8d44491861a6b5
Rodrick Kazembe <a-team@kode-blog.com>
2017-03-23 08:41:10 +0000

    Apply fixes from StyleCI

--- index.php
+++ index.php
@@ -6,28 +6,28 @@ require './bootstrap/autoload.php';
 
 $base_url = 'http://localhost:8000/php7sqlsrv/index.php';
 
-if (!isset($_GET["action"])) {
-	header("Location: $base_url?action=list");
+if (!isset($_GET['action'])) {
+    header("Location: $base_url?action=list");
 }
 
-$action = $_GET["action"];
+$action = $_GET['action'];
 
 switch ($action) {
 case 'list':
-	ItemsController::index();
-	break;
+    ItemsController::index();
+    break;
 
 case 'create':
-	ItemsController::create();
-	break;
+    ItemsController::create();
+    break;
 
 case 'edit':
-	$id = $_GET["id"];
-	ItemsController::edit($id);
-	break;
+    $id = $_GET['id'];
+    ItemsController::edit($id);
+    break;
 
 case 'delete':
-	$id = $_GET["id"];
-	ItemsController::delete($id);
-	break;
+    $id = $_GET['id'];
+    ItemsController::delete($id);
+    break;
 }
